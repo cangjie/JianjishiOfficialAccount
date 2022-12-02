8824591488efe037c83712e9485c7d8ebb8c08d0
cangjie <cangjie@hotmail.com>
2022-12-02 22:43:06 +0800

    post

--- Controllers/OfficialAccountApi.cs
+++ Controllers/OfficialAccountApi.cs
@@ -85,8 +85,8 @@ namespace OA.Controllers
             sOut.Close();
         }
         
-        [HttpGet("{unionId}")]
-        public  ActionResult<string> SendTextServiceMessage(string unionId, string content)
+        [HttpPost("{unionId}")]
+        public  ActionResult<string> SendTextServiceMessage([FromQuery]string unionId, [FromBody]string content)
         {
             unionId = Util.UrlDecode(unionId);
             User user = _db.user.Where(u => u.oa_union_id.Trim().Equals(unionId.Trim())).First();
